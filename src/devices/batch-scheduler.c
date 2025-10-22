07d6234e3dd472bed64cb1a4f52c98bc0e30a447
zeren wang <zeren@chalmers.se>
2025-10-22 11:32:11 +0200

    lab3

--- src/devices/batch-scheduler.c
+++ src/devices/batch-scheduler.c
@@ -56,6 +56,14 @@ typedef struct {
   unsigned long transfer_duration;
 } task_t;
 
+/* Global variables for bus synchronization */
+static struct lock bus_lock;                    /* Lock to protect bus state */
+static int current_slots_used;                  /* Current number of slots in use */
+static direction_t current_direction;           /* Current bus direction */
+static int priority_waiting[NUM_OF_DIRECTIONS]; /* Count of priority tasks waiting per direction */
+static int normal_waiting[NUM_OF_DIRECTIONS];   /* Count of normal tasks waiting per direction */
+static struct condition can_acquire_slot;       /* Condition variable for slot acquisition */
+
 void init_bus (void);
 void batch_scheduler (unsigned int num_priority_send,
                       unsigned int num_priority_receive,
@@ -76,13 +84,25 @@ static void transfer_data (const task_t *task);
 /* Releases the slot */
 static void release_slot (const task_t *task);
 
+static int counter;
+
 void init_bus (void) {
 
   random_init ((unsigned int)123456789);
 
   /* TODO: Initialize global/static variables,
      e.g. your condition variables, locks, counters etc */
-
+  counter = 0;
+  
+  /* Initialize synchronization primitives */
+  lock_init(&bus_lock);
+  cond_init(&can_acquire_slot);
+  current_slots_used = 0;
+  current_direction = SEND; /* Initial direction doesn't matter, will be set by first task */
+  priority_waiting[SEND] = 0;
+  priority_waiting[RECEIVE] = 0;
+  normal_waiting[SEND] = 0;
+  normal_waiting[RECEIVE] = 0;
 }
 
 void batch_scheduler (unsigned int num_priority_send,
@@ -189,6 +209,59 @@ void get_slot (const task_t *task) {
    * feel free to schedule priority tasks of the same direction,
    * even if there are priority tasks of the other direction waiting
    */
+  
+  lock_acquire(&bus_lock);
+  
+  /* Update waiting counters */
+  if (task->priority == PRIORITY) {
+    priority_waiting[task->direction]++;
+  } else {
+    normal_waiting[task->direction]++;
+  }
+  
+  /* Wait until we can acquire a slot */
+  while (1) {
+    /* Check if we can acquire a slot */
+    bool can_acquire = false;
+    
+    /* Case 1: Bus is empty - any task can acquire */
+    if (current_slots_used == 0) {
+      can_acquire = true;
+    }
+    /* Case 2: Bus has same direction and has capacity */
+    else if (current_direction == task->direction && current_slots_used < BUS_CAPACITY) {
+      /* Check priority rule: normal tasks can't acquire if priority tasks are waiting */
+      if (task->priority == PRIORITY) {
+        can_acquire = true;
+      } else if (task->priority == NORMAL) {
+        /* Normal task can only acquire if no priority tasks are waiting in any direction */
+        if (priority_waiting[SEND] == 0 && priority_waiting[RECEIVE] == 0) {
+          can_acquire = true;
+        }
+      }
+    }
+    
+    if (can_acquire) {
+      /* Acquire the slot */
+      if (current_slots_used == 0) {
+        current_direction = task->direction; /* Set bus direction */
+      }
+      current_slots_used++;
+      
+      /* Update waiting counters */
+      if (task->priority == PRIORITY) {
+        priority_waiting[task->direction]--;
+      } else {
+        normal_waiting[task->direction]--;
+      }
+      
+      lock_release(&bus_lock);
+      return;
+    }
+    
+    /* Cannot acquire slot yet, wait */
+    cond_wait(&can_acquire_slot, &bus_lock);
+  }
 }
 
 void transfer_data (const task_t *task) {
@@ -202,4 +275,19 @@ void release_slot (const task_t *task) {
    *       - Do you need to notify any waiting task?
    *       - Do you need to increment/decrement any counter?
    */
+  
+  lock_acquire(&bus_lock);
+  
+  /* Release the slot */
+  current_slots_used--;
+  
+  /* If bus becomes empty, reset direction */
+  if (current_slots_used == 0) {
+    current_direction = SEND; /* Reset to default direction */
+  }
+  
+  /* Notify all waiting tasks that a slot became available */
+  cond_broadcast(&can_acquire_slot, &bus_lock);
+  
+  lock_release(&bus_lock);
 }
